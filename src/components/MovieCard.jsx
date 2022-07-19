b12d4fa2036b98b8f3601e89ff163df4ba6095b3
Claudio Puggioni <claudieddu@hotmail.com>
2022-07-19 03:44:29 -0700

    final

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,6 +3,7 @@ function MovieCard(props) {
     <div className="movieCard" onClick={() => props.handleClick(props.id)} id={props.id} style={{ backgroundImage: `url("https://image.tmdb.org/t/p/w200${props.srcPPartial}")` }}>
       <div className="cardDetail">
         <div className="cardDetailTitle">{props.cardDetailTitle}</div>
+        <div className="cardDetailBorder"></div>
         <div className="cardDetailBody">
           <div className="cardDetailBodyTxt">{props.cardDetailBodyTxt}</div>
           <div className="cardDetailBodyRating">{props.cardDetailRating}</div>
